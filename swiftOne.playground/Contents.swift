df8c01288011b1faf60e5a7d65d67159a8b65677
LXC <1817423481@qq.com>
2016-03-09 22:20:39 +0800

    add playground

---
+++ swiftOne.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+print("hello World")
+/*
+sdd
+
+*/
+let a :Int = 10
+let minValue = UInt8.min
+let maxValue = UINT16_MAX
+let intMinValue = INT16_MAX
+
+//定义了一个刘小椿
+let maxLoginAttempts = 10
+
+
+let 刘小椿 = "傻逼";
+print(maxLoginAttempts)
+print(maxLoginAttempts)
+print("liuxiaochun是\(刘小椿)kkk")
+
+var currentLoginAttempt: Int = 1
+
+currentLoginAttempt = 5
+
+var x = 1.0,y = 2.0,z = 3.0
+
+let PI = 3.14159, SPEEODFLIGHT = 300_00
